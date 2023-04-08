4500044e358e89bbdb2c76b8350c3f0727b43bed
Aidan Cooper <aidcoope@iu.edu>
2023-04-08 14:43:44 -0400

    Spring Initializer Initial Commit

---
+++ src/main/java/edu/iu/c322/mist/categorization/CategorizationApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.mist.categorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/c322/mist/categorization/CategorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.mist.categorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
