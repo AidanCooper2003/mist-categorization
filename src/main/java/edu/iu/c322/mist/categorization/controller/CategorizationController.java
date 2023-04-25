90cac39f0b6d13a67b10cbe7a59eb9c4ba23484a
Aidan Cooper <aidcoope@iu.edu>
2023-04-25 14:37:37 -0400

    Implemented getCategories and getCategory

--- src/main/java/edu/iu/c322/mist/categorization/controller/CategorizationController.java
+++ src/main/java/edu/iu/c322/mist/categorization/controller/CategorizationController.java
@@ -1,9 +1,17 @@
 package edu.iu.c322.mist.categorization.controller;
 
+import edu.iu.c322.mist.categorization.model.CustomerProfile;
+import edu.iu.c322.mist.categorization.model.GameCategory;
 import edu.iu.c322.mist.categorization.repository.GameListRepository;
 import edu.iu.c322.mist.categorization.repository.UserListRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/download")
@@ -14,5 +22,23 @@ public class CategorizationController {
     public CategorizationController(UserListRepository userRepository, GameListRepository gameRepository){
         this.userRepository = userRepository;
         this.gameRepository = gameRepository;
+    }
+
+    @GetMapping("/{username}")
+    public List<GameCategory> getCategories(@PathVariable String username){
+        return userRepository
+                .getCustomerProfileByUsernameEquals(username)
+                .getCustomCategories();
+    }
+
+    @GetMapping("/{username}/{categoryId}")
+    public GameCategory getCategory(@PathVariable String username, @PathVariable int categoryId){
+        CustomerProfile user = userRepository.getCustomerProfileByUsernameEquals(username);
+        for (GameCategory category : user.getCustomCategories()) {
+            if (category.getId() == categoryId){
+                return category;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category of this ID does not exist for this user.");
     }
 }
--- src/main/java/edu/iu/c322/mist/categorization/repository/UserListRepository.java
+++ src/main/java/edu/iu/c322/mist/categorization/repository/UserListRepository.java
@@ -1,8 +1,9 @@
 package edu.iu.c322.mist.categorization.repository;
 
 import edu.iu.c322.mist.categorization.model.CustomerProfile;
+import edu.iu.c322.mist.categorization.model.GameCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserListRepository extends JpaRepository<CustomerProfile, Integer> {
-
+    CustomerProfile getCustomerProfileByUsernameEquals(String username);
 }
