a89dd65653334a218414b5751d98e1ff5f140efb
Aidan Cooper <aidcoope@iu.edu>
2023-04-25 14:25:21 -0400

    Added repositories to controller and added boilerplate for exception handler.

--- src/main/java/edu/iu/c322/mist/categorization/controller/CategorizationController.java
+++ src/main/java/edu/iu/c322/mist/categorization/controller/CategorizationController.java
@@ -1,10 +1,18 @@
 package edu.iu.c322.mist.categorization.controller;
 
+import edu.iu.c322.mist.categorization.repository.GameListRepository;
+import edu.iu.c322.mist.categorization.repository.UserListRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/download")
 public class CategorizationController {
-    private
+    private UserListRepository userRepository;
+    private GameListRepository gameRepository;
+
+    public CategorizationController(UserListRepository userRepository, GameListRepository gameRepository){
+        this.userRepository = userRepository;
+        this.gameRepository = gameRepository;
+    }
 }
